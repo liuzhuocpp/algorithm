d51b6cf1d83fcb5a748107792a28b44f7e843dd5
liuzhuocpp <liuzhuocpp@qq.com>
2015-01-19 16:13:59 +0800

    simple segment tree template

--- data_structure/main.cpp
+++ data_structure/main.cpp
@@ -3,10 +3,56 @@
 #include <cstring>
 
 #include "segment_tree.h"
+
 using namespace std;
 
+
+
+const int N = 50009;
+int arr[N];
+SegmentTree<int> seg;
 int main()
 {
+//    freopen("in","r",stdin);
+    int t;
+    scanf("%d",&t);
+    for(int cot=1;cot<=t;++cot)
+    {
+        printf("Case %d:\n",cot);
+        int n;
+        scanf("%d",&n);
+        for(int i=0;i<n;++i) scanf("%d",&arr[i]);
 
+        // make_tree(1,1,n);
+        seg.build(arr, n);
+        char s[100];
+        while(scanf("%s",s)==1 && s[0]!='E')
+        {
+            int a,b,v;
+            if(s[0]=='A')
+            {
+                scanf("%d%d",&a,&v);
+                a --;            
+                // modify(1,a,v);
+                
+                seg.modify(a, seg.query(a, a) + v);
+            }
+            if(s[0]=='S')
+            {
+                scanf("%d%d",&a,&v);
+                a --;
+                // modify(1,a,-v);
+                seg.modify(a, seg.query(a, a)-v);
+            }
+            if(s[0]=='Q')
+            {
+                scanf("%d%d",&a,&b);
+                a--;
+                b --;
+                // printf("%d\n",query(1,a,b));
+                printf("%d\n",seg.query(a, b));
+            }
+        }
+    }
     return 0;
 }
--- data_structure/segment_tree.h
+++ data_structure/segment_tree.h
@@ -2,26 +2,78 @@
 #define SEGMENT_TREE_H
 
 
-
 template<typename Value>
-class SegmentTreePrivate;
-template<typename Value, typename >
 class SegmentTree
 {
 public:
 
-    SegmentTree();
+    SegmentTree():c(0){}
+    SegmentTree(Value *a, int n) { build(a, n); }
+    ~SegmentTree() 
+    {
+        if(c) delete [] c;
+    }
+    void build(Value *_a, int _n)
+    {        
+        if(c == 0) c = new Value[_n * 4 + 9];
+        else if(_n > n) 
+        {            
+            delete [] c;
+            c = new Value[_n * 4 + 9];
+        }
 
-    template<typename RandomIterator>
-    SegmentTree(RandomIterator first, RandomIterator end);
-
-    template<typename RandomIterator>
-    void build(RandomIterator first, RandomIterator end);
-
-    void modify(int l, int r, const Value &c);
-    Value query(int l, int r);
+        n = _n;
+        a = _a;
+        _build(1, 0, n - 1);
+    }
+    void modify(int x, const Value &v)
+    {
+        _modify(1, 0, n - 1, x, v);
+    }
+    Value query(int l, int r)
+    {
+        return _query(1, 0, n - 1, l, r);
+    }
 private:
-    SegmentTreePrivate<Value> *seg; 
+    Value *c, *a;
+    int n;
+#define ls (p << 1)
+#define rs (ls | 1)
+#define mid ((L + R) >> 1)
+    void _build(int p, int L, int R)
+    {
+        if(L == R)
+        {
+            c[p] = a[L];
+            return ;
+        }
+        _build(ls, L, mid);
+        _build(rs, mid + 1, R);
+        c[p] = c[ls] + c[rs];
+    }
+    void _modify(int p, int L, int R, int x, const Value &v)
+    {
+        if(L == R)
+        {
+            c[p] = v;
+            return ;
+        }
+        if(x <= mid) _modify(ls, L, mid, x, v);
+        else _modify(rs, mid + 1, R, x, v);
+        c[p] = c[ls] + c[rs];
+    }
+    Value _query(int p, int L, int R, int l, int r)
+    {
+        if(l == L && r == R) return c[p];
+        if(r <= mid) return _query(ls, L, mid, l, r);
+        else if(l > mid) return _query(rs, mid + 1, R, l, r);
+        else return _query(ls, L, mid, l, mid) + 
+                    _query(rs, mid + 1, R, mid + 1, r);
+    }
+#undef mid
+#undef ls
+#undef rs
+    
 };
 
 
