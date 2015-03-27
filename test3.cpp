2a91eb8232a607eba2a64610634d51175db64376
liuzhuocpp <liuzhuocpp@qq.com>
2015-03-27 09:48:59 +0800

    add AcAutomaton

--- test3.cpp
+++ test3.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+
 #include "trie.h"
 using namespace std;
-
+using namespace lz;
+const int N = 1e6 + 9;
+char s[N];
 int main()
 {
+	int T;
+	scanf("%d", &T);
+	while(T --)
+	{
+		AcAutomaton<26> ac;
+		int n;
+		scanf("%d", &n);
+		for(int i = 0; i < n; ++ i)
+		{
+			scanf("%s", s);
+			int l = strlen(s);
+			for(int i = 0; i < l; ++ i) s[i] = s[i] - 'a';
+			ac.insert(s, s + l);
+		}
+		ac.build();
+
+		scanf("%s", s);
+		int l = strlen(s);
+		for(int i = 0; i < l; ++ i) s[i] = s[i] - 'a';
+
+		// for(int i = 0; i < l; ++ i)
+		// {
+		// 	cout << int(s[i]) << endl;
+		// }
+
+
+		
+		int ans = ac.query(s, s + l);
+
+
+		// cout << "FF " << endl;
+		printf("%d\n", ans);
+
+
+
+
+
+	}
+
+
 
 
 	return 0;
--- trie.h
+++ trie.h
@@ -3,10 +3,14 @@
 
 #include <vector>
 #include <queue>
+#include <iostream>
 
 namespace lz {
 
 using std::queue;
+using std::vector;
+using std::cout;
+using std::endl;
 
 template<int N>
 class Trie
@@ -56,7 +60,7 @@ public:
 		q.push(root);
 		while(!q.empty())
 		{
-			Node *c = q.top();
+			Node *c = q.front();
 			q.pop();
 			for(int i = 0; i < N; ++ i)
 			{
@@ -67,6 +71,119 @@ public:
 		}
 	}
 };
+
+
+
+
+
+
+
+
+
+template<int N>
+struct AcAutomaton
+{
+
+	struct Node
+	{
+		Node *son[N];
+		Node *next;
+		int tail;
+		Node():tail(0), next(nullptr)
+		{
+			for(int i = 0; i < N; ++ i) 
+				son[i] = nullptr;
+		}
+	};
+	Node *root;	
+	vector<Node*> v;
+	AcAutomaton():root(new Node()) { v.push_back(root); }
+	
+	template<typename RandomIterator>
+	void insert(RandomIterator first, RandomIterator end)
+	{
+		RandomIterator s = first;
+		int n = end - first;
+
+		Node *top = root;
+		for(int i = 0; i < n; ++ i)
+		{
+			if(top->son[s[i]] == nullptr)
+			{
+				top->son[s[i]] = new Node();
+				v.push_back(top->son[s[i]]);
+			}
+			top = top->son[s[i]];
+		}
+		top->tail ++;
+	}
+	void build() // bfs
+	{
+		queue<Node*> q;
+		root->next = root;
+		q.push(root);
+		while(!q.empty())
+		{
+			Node *u = q.front();
+			q.pop();
+			for(int i = 0; i < N; ++ i)
+			{
+				if(u->son[i] == nullptr)
+				{
+					if(u == root) u->son[i] = root;
+					else u->son[i] = u->next->son[i];					
+				}
+				else 
+				{
+					if(u == root) u->son[i]->next = root;
+					else u->son[i]->next = u->next->son[i];
+					q.push(u->son[i]);
+				}				
+			}
+		}
+	}
+
+
+
+	template<typename RandomIterator>	
+	int query(RandomIterator first, RandomIterator end)
+	{
+		RandomIterator s = first;
+		int n = end - first;
+
+		Node *u = root;
+		int ans = 0;
+		for(int i = 0; i < n; ++ i)
+		{
+			// cout << "JJJ" << int(s[i]) << " " << u << endl;
+			u = u->son[s[i]];
+			Node *tp = u;
+			for(;tp != root; tp = tp->next) 
+			ans += tp->tail, tp->tail = 0;
+		}
+		return ans;
+	}
+	~AcAutomaton()
+	{
+		for(int i = 0; i < int(v.size()); ++ i) delete v[i];
+	}
+	
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
