725a54e4bc683683964c910a36a0426222e6faff
liuzhuocpp <liuzhuocpp@qq.com>
2015-06-02 14:46:44 +0800

    property may be ok!

--- examples/property/main2.cpp
+++ examples/property/main2.cpp
@@ -21,7 +21,30 @@ int main()
 
     Property<tag_char, char, 
     Property<tag_int, int, 
-    Property<tag_double, double> > > cnt('z', 123, 111.1);
+    Property<tag_double, double> > > cnt('z', 123
+        , 11.11
+        );
+
+    cout << get<tag_char>(cnt) << endl;
+    get<tag_char>(cnt) = '^';
+
+
+    cout << get<tag_double>(cnt) << endl;
+    get<tag_double>(cnt) = 22.211;
+
+    cout << get<tag_double>(cnt) << endl;
+    cout << get<tag_char>(cnt) << endl;
+
+
+    typedef 
+    Property<tag_char, char, 
+    Property<tag_int, int, 
+    Property<tag_double, double> > > PP;
+
+    const PP aa('$', 12309, 91023.1223);
+
+    cout << get<tag_char>(aa) << endl;
+    // get<tag_char>(aa) = '1';
 
 
 
--- property.h
+++ property.h
@@ -22,48 +22,27 @@ struct NoProperty {};
     namespace PropertyPrivate {
 
         template<typename P>
-        struct CountProperty:
-            public integral_constant<
-                size_t, 
-                CountProperty<typename P::NextProperty>::value + 1
-            >
-        {
-            // static constexpr int num = CountProperty<typename P::NextProperty>::num + 1;
-        };
+        struct CountProperty
+        : public integral_constant<size_t, CountProperty<typename P::NextProperty>::value + 1>
+        { };
 
         template<>
-        struct CountProperty<NoProperty>:
-            public integral_constant<
-                size_t, 
-                0
-            >
-        {
-            
-        };
+        struct CountProperty<NoProperty>
+        : public integral_constant<size_t, 0>
+        { };
 
         template<typename... T>
         struct CountVariadic;
 
-
         template<typename Head, typename...T>
-        struct CountVariadic<Head, T...>:
-            public integral_constant<
-                size_t, 
-                CountVariadic<T...>::value + 1
-            >
-        {
-
-        };
+        struct CountVariadic<Head, T...>
+        : public integral_constant< size_t, CountVariadic<T...>::value + 1>
+        { };
 
         template<>
-        struct CountVariadic<>:
-            public integral_constant<
-                size_t,
-                0
-            >
-        {
-            // static constexpr int num = 0;
-        };
+        struct CountVariadic<>
+        : public integral_constant<size_t, 0>
+        { };
 
 
 
@@ -72,44 +51,104 @@ struct NoProperty {};
 
 
 
-template<typename _Tag, typename _Type, typename _NextProperty = NoProperty>
+template<typename _Tag, typename _Value, typename _NextProperty = NoProperty>
 struct Property
 {
     typedef _Tag Tag;
-    typedef _Type Type;
+    typedef _Value Value;
     typedef _NextProperty NextProperty;
 
     Property(){}
 
     template<typename Head, typename... Args>
-    Property(const Head &_val, Args ...args): val(_val), np(args...)
+    Property(const Head &value, Args ...args): m_value(value), np(args...)
     {        
         static_assert(PropertyPrivate::CountProperty<Property>::value == 
                       PropertyPrivate::CountVariadic<Head, Args...>::value,
                        "Parameters number is not equal");
     }
-    _Type val;
+    _Value m_value;
     _NextProperty np;
 };
 
-template<typename _Tag, typename _Type>
-struct Property<_Tag, _Type, NoProperty>
+template<typename _Tag, typename _Value>
+struct Property<_Tag, _Value, NoProperty>
 {
     typedef _Tag Tag;
-    typedef _Type Type;
+    typedef _Value Value;
     typedef NoProperty NextProperty;
+
     Property(){}
+    Property(const Value &value):m_value(value){}
 
-    Property(const _Type &_val):val(_val){}
-    _Type val;
+    _Value m_value;    
 };
-
 
     namespace PropertyPrivate {
 
+        template<typename QueryTag, typename Tag, typename Property>
+        struct Get
+        {
+            typedef typename Property::NextProperty  NextProperty;
+
+            static auto get(Property &p) 
+            -> decltype(Get<QueryTag, typename NextProperty::Tag, NextProperty>::get(p.np))
+            {
+                return Get<QueryTag, typename NextProperty::Tag, NextProperty>::get(p.np);
+            }
+
+            static auto get(const Property &p) 
+            -> decltype(Get<QueryTag, typename NextProperty::Tag, NextProperty>::get(p.np))
+            {
+                return Get<QueryTag, typename NextProperty::Tag, NextProperty>::get(p.np);
+            }    
+
+        };
+
+        template<typename QueryTag, typename Property>
+        struct Get<QueryTag, QueryTag, Property>
+        {
+            typedef typename Property::Value Value;
+
+            static Value& get(Property &p)
+            {
+                return p.m_value;
+            }
+
+            static const Value& get(const Property &p)
+            {
+                return p.m_value;
+            }
+        };
 
 
     } // namespace PropertyPrivate
+
+
+
+
+
+
+template<typename QueryTag, typename Tag, typename Value, typename NextProperty>
+auto get(Property<Tag, Value, NextProperty> & p) 
+-> decltype(PropertyPrivate::Get<QueryTag, Tag, Property<Tag, Value, NextProperty> >::get(p))
+{
+    return PropertyPrivate::Get<QueryTag, Tag, Property<Tag, Value, NextProperty> >::get(p);
+}
+
+template<typename QueryTag, typename Tag, typename Value, typename NextProperty>
+auto get(const Property<Tag, Value, NextProperty> & p) 
+-> decltype(PropertyPrivate::Get<QueryTag, Tag, Property<Tag, Value, NextProperty> >::get(p))
+{
+    return PropertyPrivate::Get<QueryTag, Tag, Property<Tag, Value, NextProperty> >::get(p);
+}
+
+
+
+
+
+
+
 
 
 
