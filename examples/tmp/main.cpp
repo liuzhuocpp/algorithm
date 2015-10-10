753bbff00327815e1b312d36928523ac87f28718
liuzhuocpp <liuzhuocpp@qq.com>
2015-10-10 10:30:05 +0800

    add tmp

---
+++ examples/tmp/main.cpp
@@ -0,0 +1,13 @@
+
+
+void f()
+{
+	char *a[2] = {
+			"222",
+			"3333fadasfasdfasd",
+	};
+
+
+}
+
+
---
+++ examples/tmp/main.s
@@ -0,0 +1,27 @@
+	.file	"main.cpp"
+	.section .rdata,"dr"
+LC0:
+	.ascii "222\0"
+LC1:
+	.ascii "3333fadasfasdfasd\0"
+	.text
+	.globl	__Z1fv
+	.def	__Z1fv;	.scl	2;	.type	32;	.endef
+__Z1fv:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$LC0, -8(%ebp)
+	movl	$LC1, -4(%ebp)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.ident	"GCC: (GNU) 4.8.1"
