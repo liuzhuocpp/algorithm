cc9a1c968ba8133b25e09bb86c10d5c03c5df82d
liuzhuocpp <liuzhuocpp@qq.com>
2016-12-09 14:33:13 +0800

    add testUndirectedGraph

---
+++ examples/dijkstra_shortest_paths/testUndirectedGraph.cpp
@@ -0,0 +1,63 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2016年12月8日
+ *      Author: LZ
+ */
+
+
+#include <bits/stdc++.h>
+#include "lz/dijkstra_shortest_paths.h"
+#include "lz/adjacency_list.h"
+using namespace lz;
+using namespace std;
+
+
+
+int n = 10;
+
+int main()
+{
+
+    using EP = Property<EdgeWeightTag, int>;
+
+    using G = AdjacencyList<UndirectedGraphTag, NoProperty, EP>;
+    G g(n);
+    using namespace DijkstraShortestPathsKeywords;
+
+    vector<int> p(10, -1);
+
+    g.addEdge(1, 0, EP(2));
+    g.addEdge(2, 1, EP(2));
+    g.addEdge(2, 0, EP(1));
+//    g.addEdge(2, 4, EP(98));
+//    g.addEdge(0, 3, EP(10));
+//    g.addEdge(3, 5, EP(33));
+//    g.addEdge(5, 6, EP(440));
+//    g.addEdge(6, 7, EP(110));
+//    g.addEdge(7, 8, EP(1340));
+//    g.addEdge(4, 8, EP(1055));
+//    g.addEdge(0, 8, EP(33));
+
+    dijkstraShortestPaths(g, 0, (
+
+            DijkstraShortestPathsKeywords::edgeRelaxed = [&](G::EdgeDescriptor e, G::VertexDescriptor u)
+            {
+                G::VertexDescriptor other = opposite(g, e, u);
+                p[other] = u;
+            }
+          ));
+
+    for(int i = 0; i < n; ++ i)
+    {
+        cout << "tree edge: " << i << " " << p[i] << endl;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
