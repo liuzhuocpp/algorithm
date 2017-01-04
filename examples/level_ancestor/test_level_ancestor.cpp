b5328aa8e00a91d9ae75917c63923bb48a53b7ea
liuzhuocpp <liuzhuocpp@qq.com>
2017-01-04 17:14:48 +0800

    add level_ancestor and remove  include(CTest) enable_testing ()  in subdirectory

---
+++ examples/level_ancestor/test_level_ancestor.cpp
@@ -0,0 +1,12 @@
+
+#include <bits/stdc++.h>
+
+
+using namespace lz;
+using namespace std;
+
+
+int main()
+{
+	return 0;
+}
---
+++ lz/level_ancestor.h
@@ -0,0 +1,20 @@
+/*
+ * level_ancestor.h
+ *
+ *  Created on: 2017年1月4日
+ *      Author: LZ
+ */
+
+#ifndef LZ_LEVEL_ANCESTOR_H_
+#define LZ_LEVEL_ANCESTOR_H_
+
+
+
+namespace lz{
+
+
+
+
+}// namesapce lz
+
+#endif /* LZ_LEVEL_ANCESTOR_H_ */
