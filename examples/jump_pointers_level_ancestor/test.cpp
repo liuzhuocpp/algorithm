b161dc639e660e3b33dc466d7172b53813cbbd79
liuzhuocpp <liuzhuocpp@qq.com>
2017-01-14 15:43:13 +0800

    jump_pointers_level_ancestor ok

---
+++ examples/jump_pointers_level_ancestor/test.cpp
@@ -0,0 +1,74 @@
+
+#include <bits/stdc++.h>
+#include <lz/jump_pointers_level_ancestor.h>
+#include <lz/adjacency_list.h>
+
+using namespace lz;
+using namespace std;
+
+int n = 10;
+template<typename G, typename V>
+void outputOutEdges(const G &g,  V u)
+{
+    cout << "outEdges: " << u << ": " ;
+    for(auto e: outEdges(g, u))
+    {
+        cout << opposite(g, e, u) << " , " ;
+    }
+    cout << endl;
+}
+int main()
+{
+    using G = AdjacencyList<UndirectedGraphTag>;
+    using V = typename GraphTraits<G>::VertexDescriptor;
+    G g(n);
+    addEdge(g, 0, 1);
+    addEdge(g, 1, 2);
+    addEdge(g, 1, 3);
+    addEdge(g, 1, 4);
+    addEdge(g, 0, 5);
+
+    addEdge(g, 5, 6);
+    addEdge(g, 6, 7);
+    addEdge(g, 7, 8);
+    addEdge(g, 8, 9);
+
+
+
+    auto getAns = jumpPointersLevelAncestor(g);
+
+    cout << getAns(3, 0) << endl;
+    cout << getAns(2, 1) << endl;
+    cout << getAns(9, 2) << endl;
+    cout << getAns(9, 3) << endl;
+    cout << getAns(9, 4) << endl;
+    cout << getAns(9, 5) << endl;
+    cout << getAns(9, 6) << endl;
+
+    assert(getAns(3, 0) == 0);
+    assert(getAns(2, 1) == 1);
+    assert(getAns(9, 2) == 6);
+    assert(getAns(9, 3) == 7);
+    assert(getAns(9, 4) == 8);
+    assert(getAns(9, 5) == 9);
+    assert(getAns(9, 6) == -1);
+
+
+//    cout << getAns(5, 2) << endl;
+
+
+
+
+//    cout << getAns(5, 2) << endl;
+//    cout << getAns(4, 3) << endl;
+//
+//    assert(getAns(0, 1) == 0);
+//    assert(getAns(5, 2) == 0);
+//    assert(getAns(4, 3) == 1);
+
+
+
+
+
+    return 0;
+}
---
+++ lz/jump_pointers_level_ancestor.h
@@ -0,0 +1,93 @@
+/*
+ * level_ancestor.h
+ *
+ *  Created on: 2017年1月4日
+ *      Author: LZ
+ */
+
+#ifndef LZ_JUMP_POINTERS_LEVEL_ANCESTOR_H_
+#define LZ_JUMP_POINTERS_LEVEL_ANCESTOR_H_
+
+#include <lz/parameter.h>
+#include <lz/depth_first_search.h>
+#include <lz/iterator_range.h>
+#include <lz/graph_utility.h>
+#include <cmath>
+#include <vector>
+
+
+namespace lz {
+
+    namespace JumpPointersLevelAncestorKeywords {
+
+
+    LZ_PARAMETER_KEYWORD(tag, rootVertex)
+    LZ_PARAMETER_KEYWORD(tag, vertexIndexMap)
+    LZ_PARAMETER_KEYWORD(tag, depthMap)
+
+    } // namespace jumpPointersLowestCommonAncestorKeywords
+
+
+
+template<typename Tree, typename Params = EmptyParamPack>
+auto jumpPointersLevelAncestor(const Tree& tree, const Params& params = Params())
+{
+    namespace Keys = JumpPointersLevelAncestorKeywords;
+    using Vertex = typename GraphTraits<Tree>::VertexDescriptor;
+
+    auto n = verticesNumber(tree);
+    auto _vertices = vertices(tree);
+    auto rootVertex = params[Keys::rootVertex | *_vertices.first];
+    auto indexMap = params[Keys::vertexIndexMap | vertexPropertyMap(tree, vertexIndexTag)];
+    auto depthMap = params[Keys::depthMap || calculateVertexIndexComposeMap<size_t>(indexMap, n)];
+    using DepthType = typename MapTraits<decltype(depthMap)>::ValueType;
+
+    int maxDepth = std::log2(n);
+    std::vector<std::vector<Vertex>> p(n, std::vector<Vertex>(maxDepth + 1));
+
+    depthMap[rootVertex] = 0;
+    for(auto j: irange(maxDepth + 1))
+        p[rootVertex][j] = rootVertex;
+
+    adjacencyTreeDFS(tree, (
+            DepthFirstSearchKeywords::treeEdge = [&](Vertex u, Vertex to) {
+                p[to][0] = u;
+                for(auto i: irange(1, maxDepth + 1))
+                {
+                    p[to][i] = p[p[to][i - 1]][i - 1];
+                }
+                depthMap[to] = depthMap[u] + 1;
+            }
+            ));
+
+    return [=, p = std::move(p)](Vertex u, DepthType d) {
+
+        if(depthMap[u] < d) return GraphTraits<Tree>::nullVertex();
+        if(depthMap[u] == d) return u;
+        for(auto i: irange(maxDepth, -1, -1))
+        {
+            if(depthMap[p[u][i]] >= d)
+            {
+                u = p[u][i];
+            }
+        }
+
+        return u;
+
+
+
+    };
+
+
+
+
+}
+
+
+
+
+
+
+}// namesapce lz
+
+#endif /* LZ_JUMP_POINTERS_LEVEL_ANCESTOR_H_ */
