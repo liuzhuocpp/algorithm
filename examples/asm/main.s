4b69338fb08243099354dc199a437fa8ecc61a1d
liuzhuocpp <liuzhuocpp@qq.com>
2017-04-08 18:46:52 +0800

    add asm example

---
+++ examples/asm/main.s
@@ -0,0 +1,19 @@
+
+.LC0:
+	.ascii "hello sswerwerwer !!!\0"
+
+	.globl	main
+
+	.seh_proc	main
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$32, %rsp
+	call	__main
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
