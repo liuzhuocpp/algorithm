55d7ed3244e8dffe513d646a7d6fe63c13c690c7
liuzhuocpp <liuzhuocpp@qq.com>
2017-04-13 14:28:44 +0800

    testNfa改名字 为test_nfa

--- examples/compiler/test_nfa.cpp
+++ examples/compiler/test_nfa.cpp
@@ -11,32 +11,9 @@ using namespace std;
 
 int main()
 {
-
-//    std::optional<int> a;
-
-
     string s = "a*b|c|dj*kk";
-//    s = "a";
-
 
     NFA<char> nfa = analyzeRegex(s.begin(), s.end());
-//    cout << nfa << endl;
-
-
-//    for(int i = 0; i < 10; ++ i)
-//    {
-//        cout << "I: " << i << ": ";
-//        for(auto e: outEdges(nfa, i))
-//        {
-//            cout << "(" <<
-//                    source(nfa, e) << ", " << target(nfa, e) <<  ")|";
-//        }
-//        cout << endl;
-//
-//    }
-
-
-
 
     string text;
     text =("aaaaaaaaaaaaaaaab");
@@ -57,21 +34,6 @@ int main()
     text =("djjjjjjjjjkk");
     cout << simulateNFA(nfa, text.begin(), text.end()) << endl;
 
-//    auto nfaMap = edgePropertyMap(nfa, nfaEdgeTag);
-//
-//    depthFirstSearch(nfa, (
-//        DepthFirstSearchKeywords::enterVertex = nfa.start,
-//        DepthFirstSearchKeywords::treeEdge = [=](auto edge, auto u, auto to)
-//        {
-//            cout << u << " " << to << " ";
-//            if(nfaMap[edge] != nullptr) cout << *nfaMap[edge];
-//            cout << endl;
-//        }));
-
-
-
-//
-//    cout << string(100, '-') << endl;
 
 
 	return 0;
