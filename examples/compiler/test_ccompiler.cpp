e5e61b202a0648b0e2ca53e23f0f9f620d4164a3
liuzhuocpp <liuzhuocpp@qq.com>
2017-04-17 22:13:23 +0800

    c compiler   词法分析 better

--- examples/compiler/test_ccompiler.cpp
+++ examples/compiler/test_ccompiler.cpp
@@ -17,6 +17,7 @@ struct LexicalSymbol
     {
         Identifier,
         Integer,
+
         Plus,
         Minus,
         Asterisk,
@@ -44,7 +45,23 @@ struct LexicalSymbol
         EqualMark,
 
         End,
+
+        While,
+        If,
+        Int,
+        Float,
+        Double,
+        Return,
+        For,
+
     };
+
+    static const
+    map<string, Type> keywordToType;
+
+    static const
+    map<Type, pair<string, string> > namesAndRegexs;
+
     constexpr static const char* names[] = {
         "identifier",
         "integer",
@@ -101,14 +118,55 @@ struct LexicalSymbol
     LexicalSymbol(Type type, string value):type(type), value(value){}
     LexicalSymbol() = default;
     LexicalSymbol(Type type): type(type){}
+    LexicalSymbol(string s)
+    {
+        if(s[0] == '_' || islower(s[0]) || isupper(s[0]))
+        {
+            if(keywordToType.count(s))
+            {
+                type = keywordToType.at(s);
+                value = s; // can be improved
+            }
+            else
+            {
+                type = Type::Identifier;
+                value = s;
+            }
+
+        }
+        else if(isdigit(s[0]))
+        {
+            type = Type::Integer;
+            value = s;
+        }
+        else
+        {
+            for(int i = static_cast<int>(Type::Integer) + 1; i < static_cast<int>(Type::End); ++ i)
+            {
+                if(s == names[i])
+                {
+                    type = static_cast<Type>(i);
+                    return ;
+                }
+            }
+            assert(0);
+        }
+    }
 
     template <class Char, class Traits>
     friend std::basic_ostream<Char, Traits>&
     operator<<(std::basic_ostream<Char, Traits>& os,
                const LexicalSymbol&  ls)
     {
+        if(namesAndRegexs.count(ls.type))
+            os << namesAndRegexs.at(ls.type).first;
+        else
+        {
+            os << ls.value;
+        }
 
-        os << names[static_cast<unsigned>(ls.type)];
+
+//        os << names[static_cast<unsigned>(ls.type)];
 
         if(ls.type == Type::Identifier || ls.type == Type::Integer)
         {
@@ -117,9 +175,46 @@ struct LexicalSymbol
         return os;
 
     }
+};
+
+const map<LexicalSymbol::Type, pair<string, string> > LexicalSymbol::namesAndRegexs =
+{
+        { Type::Identifier, {"identifier", "(_|[a-zA-Z])(_|[a-zA-Z0-9])*", }},
+        { Type::Integer, {"integer", "([0-9][0-9]*)", }},
+        { Type::Plus, {"+", R"(\+)", }},
+        { Type::Minus, {"-", R"(\-)", }},
+        { Type::Asterisk, {"*", R"(\*)", }},
+        { Type::Slash, {"/", R"(\/)", }},
+        { Type::Ampersand, {"&", R"(\&)", }},
+        { Type::DoubleAmpersand, {"&&", R"(\&\&)", }},
+        { Type::VerticalBar, {"|", R"(\|)", }},
+        { Type::DoubleVerticalBar, {"||", R"(\|\|)", }},
+        { Type::ExclamationMark, {"!", R"(\!)", }},
+        { Type::LeftParenthese, {"(", R"(\()", }},
+        { Type::RightParenthese, {")", R"(\))", }},
+        { Type::LeftSquareBracket, {"[", R"(\[)", }},
+        { Type::RightSquareBracket, {"]", R"(\])", }},
+        { Type::LeftBrace, {"{", R"(\{)", }},
+        { Type::RightBrace, {"}", R"(\})", }},
+        { Type::Semicolon, {";", R"(\;)", }},
+        { Type::AssignMark, {"=", R"(\=)", }},
+        { Type::EqualMark, {"==", R"(\=\=)", }},
+};
+
+const map<string, LexicalSymbol::Type> LexicalSymbol::keywordToType =
+{
+    {"while", Type::While},
+    {"if", Type::If},
+    {"int", Type::Int},
+    {"float", Type::Float},
+    {"double", Type::Double},
+    {"return", Type::Return},
+    {"for", Type::For},
 
 
 };
+
+
 template<typename Iterator>
 auto lexicalAnalyze(Iterator textBegin, Iterator textEnd)
 {
@@ -134,7 +229,7 @@ auto lexicalAnalyze(Iterator textBegin, Iterator textEnd)
             regexAndFuncs;
 
     auto func = [&](auto first, auto last) {
-        ans.push_back(LexicalSymbol(LexicalSymbol::Type::Identifier, string(first, last)));
+        ans.push_back(LexicalSymbol(string(first, last)));
     };
 
     for(unsigned i: irange(static_cast<unsigned>(LexicalSymbol::Type::End) ) )
@@ -185,8 +280,19 @@ auto lexicalAnalyze(Iterator textBegin, Iterator textEnd)
     }
 
     return ans;
+}
+
+
+template<typename InputIterator>
+void grammarAnalyze(InputIterator first, InputIterator last)
+{
 
 }
+
+
+
+
+
 
 
 int main()
