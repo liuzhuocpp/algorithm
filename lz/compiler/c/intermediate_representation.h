62b2dc912e249dbd2a1c3bec528853574617d083
liuzhuocpp <liuzhuocpp@qq.com>
2017-04-28 11:47:16 +0800

    add ostream for IntermediateRepresentation

--- lz/compiler/c/intermediate_representation.h
+++ lz/compiler/c/intermediate_representation.h
@@ -15,11 +15,19 @@ namespace lz {
 struct IntermediateRepresentation
 {
     std::string op, arg1, arg2, res;
-    int label = -1;// 可选的label, -1 表示无label
+    int label = -1;// 可选的label, -1 表示无label， label是自增的
     IntermediateRepresentation() = default;
     IntermediateRepresentation(std::string op, std::string arg1, std::string arg2, std::string res):
         op(op), arg1(arg1), arg2(arg2), res(res){}
 
+    template <class Char, class Traits>
+    friend std::basic_ostream<Char, Traits>&
+    operator<<(std::basic_ostream<Char, Traits>& os,
+               const IntermediateRepresentation& IR)
+    {
+        os << op << " " << arg1 << " " << arg2 << " " << res;
+        return os;
+    }
 
 };
 
@@ -33,8 +41,24 @@ struct IRTable: std::vector<IntermediateRepresentation>
     template <class Char, class Traits>
     friend std::basic_ostream<Char, Traits>&
     operator<<(std::basic_ostream<Char, Traits>& os,
-               const IRTable& IR)
+               const IRTable& table)
     {
+        int maxWidth = 0;
+        for(auto IR: table)
+        {
+            maxWidth = std::max(maxWidth, std::to_string(IR.label).size());
+        }
+
+        for(auto IR: table)
+        {
+            if(maxWidth != 0)
+            {
+                auto label = std::to_string(IR.label);
+                os << "L" << label << ":" << std::string(maxWidth - label.size(), ' ');
+            }
+
+            os << IR << std::endl;
+        }
 
         return os;
     }
