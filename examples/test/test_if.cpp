3e97c63c50efc834f5846f427862ae0611ca7dda
liuzhuocpp <liuzhuocpp@qq.com>
2017-05-04 16:38:12 +0800

    add test if.cpp

---
+++ examples/test/test_if.cpp
@@ -0,0 +1,24 @@
+
+
+void f(int a)
+{
+
+    if(a > 0)
+    {
+        a ++;
+    }
+    else if(a > -1)
+    {
+        a --;
+    }
+    else if(a == 33)
+    {
+        a += 3;
+    }
+    else if(a == 55)
+    {
+        a += 5;
+    }
+
+
+}
---
+++ examples/test/test_if.s
@@ -0,0 +1,62 @@
+	.text
+	.def	 _Z1fi;
+	.scl	2;
+	.type	32;
+	.endef
+	.globl	_Z1fi
+	.p2align	4, 0x90
+_Z1fi:                                  # @_Z1fi
+.Lcfi0:
+.seh_proc _Z1fi
+# BB#0:                                 # %entry
+	pushq	%rax
+.Lcfi1:
+	.seh_stackalloc 8
+.Lcfi2:
+	.seh_endprologue
+	movl	%ecx, 4(%rsp)
+	cmpl	$0, 4(%rsp)
+	jle	.LBB0_2
+# BB#1:                                 # %if.then
+	movl	4(%rsp), %eax
+	addl	$1, %eax
+	movl	%eax, 4(%rsp)
+	jmp	.LBB0_11
+.LBB0_2:                                # %if.else
+	cmpl	$-1, 4(%rsp)
+	jle	.LBB0_4
+# BB#3:                                 # %if.then2
+	movl	4(%rsp), %eax
+	addl	$-1, %eax
+	movl	%eax, 4(%rsp)
+	jmp	.LBB0_10
+.LBB0_4:                                # %if.else3
+	cmpl	$33, 4(%rsp)
+	jne	.LBB0_6
+# BB#5:                                 # %if.then5
+	movl	4(%rsp), %eax
+	addl	$3, %eax
+	movl	%eax, 4(%rsp)
+	jmp	.LBB0_9
+.LBB0_6:                                # %if.else6
+	cmpl	$55, 4(%rsp)
+	jne	.LBB0_8
+# BB#7:                                 # %if.then8
+	movl	4(%rsp), %eax
+	addl	$5, %eax
+	movl	%eax, 4(%rsp)
+.LBB0_8:                                # %if.end
+	jmp	.LBB0_9
+.LBB0_9:                                # %if.end10
+	jmp	.LBB0_10
+.LBB0_10:                               # %if.end11
+	jmp	.LBB0_11
+.LBB0_11:                               # %if.end12
+	popq	%rax
+	retq
+	.seh_handlerdata
+	.text
+.Lcfi3:
+	.seh_endproc
+
+
