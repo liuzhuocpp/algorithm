b4f9df0393abb6a158ac49e1dda696a6a0241aad
liuzhuocpp <liuzhuocpp@qq.com>
2017-05-06 15:01:43 +0800

    add instruction.def, make ThreeAddressInstruction better

--- lz/compiler/c/three_address_instruction.h
+++ lz/compiler/c/three_address_instruction.h
@@ -14,22 +14,79 @@ namespace lz {
 
 struct ThreeAddressInstruction
 {
-    std::string op, arg1, arg2, res;
-//    int label = -1;// 可选的label, -1 表示无label， label是自增的
+    enum class Category
+    {
+#define X(cat, name) cat,
+#include <lz/compiler/c/instruction.def>
+#undef X
+        Unknown,
+    };
+
+
+    static const std::unordered_map<std::string, Category> NameToCategory;
+    static constexpr const char* Names[] =
+    {
+
+#define X(cat, name) name,
+#include <lz/compiler/c/instruction.def>
+#undef X
+        "unknown",
+
+    };
+
+
+private:
+    Category category = Category::Unknown;
+    std::string  m_arg1, m_arg2, m_res;
+//    arg1()
+public:
+    std::string& result()
+    {
+        return m_res;
+    }
+
+
     ThreeAddressInstruction() = default;
-//    ThreeAddressInstruction(std::string op, std::string arg1, std::string arg2, std::string res):
-//        op(op), arg1(arg1), arg2(arg2), res(res){}
+
+    ThreeAddressInstruction(std::string op, std::string arg1, std::string arg2, std::string res):
+        m_arg1(arg1), m_arg2(arg2), m_res(res)
+    {
+        if(auto it = NameToCategory.find(op); it != NameToCategory.end())
+        {
+            category = it->second;
+        }
+        else
+        {
+            assert(0);
+        }
+
+    }
 
     template <class Char, class Traits>
     friend std::basic_ostream<Char, Traits>&
     operator<<(std::basic_ostream<Char, Traits>& os,
                const ThreeAddressInstruction& IR)
     {
-        os << IR.op << " " << IR.arg1 << " " << IR.arg2 << " " << IR.res;
+        os << IR.Names[static_cast<unsigned>(IR.category)] << " " << IR.m_arg1 << " " << IR.m_arg2 << " " << IR.m_res;
         return os;
     }
 
 };
+
+
+const std::unordered_map<std::string, ThreeAddressInstruction::Category>
+    ThreeAddressInstruction::NameToCategory =
+{
+
+
+#define X(cat, name) {name, ThreeAddressInstruction::Category::cat},
+#include <lz/compiler/c/instruction.def>
+#undef X
+
+
+};
+
+
 
 struct ThreeAddressCode: private std::vector<ThreeAddressInstruction>
 {
@@ -50,7 +107,7 @@ public:
 
     void generateCode(std::string op, std::string arg1, std::string arg2, std::string res)
     {
-        this->push_back({op, arg1, arg2, res});
+        this->push_back(ThreeAddressInstruction(op, arg1, arg2, res));
     };
 
     template<typename Iterator>
@@ -60,7 +117,7 @@ public:
             this->labels.insert(instructionId);
         for(;first != last; first ++)
         {
-            (*this)[*first].res = "L" + std::to_string(instructionId);
+            (*this)[*first].result() = "L" + std::to_string(instructionId);
 
 
         }
@@ -84,10 +141,12 @@ public:
         for(auto i : lz::irange(table.size()))
         {
             if(table.labels.count(i))
+            {
                 maxWidth = std::max(maxWidth, (int)std::to_string(i).size());
+            }
         }
 
-        constexpr const int paddingSpaceNumber = 3;
+        constexpr int paddingSpaceNumber = 3;
         for(auto i : lz::irange(table.size()))
         {
 
@@ -104,6 +163,7 @@ public:
             }
             os  << std::string(paddingSpaceNumber, ' ') <<  table[i] << std::endl;
         }
+
         if(table.labels.count(table.size()))
         {
             os << "L" << table.size() << ":\n";
