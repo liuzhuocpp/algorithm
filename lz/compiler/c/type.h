b21e4a446a7e1f6ceaea0c1d9cf55fb9c73c8007
liuzhuocpp <liuzhuocpp@qq.com>
2017-05-07 09:32:59 +0800

    improve type.h

--- lz/compiler/c/type.h
+++ lz/compiler/c/type.h
@@ -22,20 +22,37 @@ struct Type
         Float,
         Double,
 
-//        Array,
+        Array,
 //        Struct,
         Unknown,
     };
 
-public:
-
-
 
 private:
-    Category category;
-
+    Category m_category;
     std::vector<int> arrayDimensions;
 public:
+
+    Category getCategory() const
+    {
+        if(arrayDimensions.empty())
+        {
+            return m_category;
+        }
+        else
+        {
+            return Category::Array;
+        }
+    }
+    Category baseType() const
+    {
+        return m_category;
+    }
+
+
+
+
+
     int getWidth() const
     {
         int ans = 1; // 暂时先设为1
@@ -54,7 +71,7 @@ public:
 
     Type subArray() const
     {
-        Type ans(this->category);
+        Type ans(this->m_category);
         ans.arrayDimensions.assign(this->arrayDimensions.begin() + 1, this->arrayDimensions.end());
         return ans;
     }
@@ -62,15 +79,15 @@ public:
 
 
     Type(Category cat = Category::Unknown):
-        category(cat){}
+        m_category(cat){}
 
 
 
 
-    operator Category()
-    {
-        return category;
-    }
+//    operator Category()
+//    {
+//        return m_category;
+//    }
 
     template <class Char, class Traits>
     friend std::basic_ostream<Char, Traits>&
@@ -81,9 +98,9 @@ public:
         os << "Type(";
 
 
-        if(id.category == Category::Float) os << "float" ;
-        else if(id.category == Category::Int) os << "int";
-        else if(id.category == Category::Double) os << "double";
+        if(id.m_category == Category::Float) os << "float" ;
+        else if(id.m_category == Category::Int) os << "int";
+        else if(id.m_category == Category::Double) os << "double";
         else os << "unknown";
 
         if(!id.arrayDimensions.empty())
